e3ac4844070b992cd742aebf4898bd3e9b5dfb71
ABCambridge <andrewbcambridge@gmail.com>
2025-02-11 10:41:59 -0700

    81% tests passing

--- tests/pizza.spec.js
+++ tests/pizza.spec.js
@@ -10,23 +10,33 @@ test( "home page", async ({ page }) => {
 
 test( "login and order pizza", async ( { page } ) => {
     await page.goto('http://localhost:5173/');
+
+    // Login
     await page.getByRole('link', { name: 'Login' }).click();
     await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
     await page.getByRole('textbox', { name: 'Email address' }).press('Tab');
     await page.getByRole('textbox', { name: 'Password' }).fill('admin');
     await page.getByRole('textbox', { name: 'Password' }).press('Enter');
-    await page.getByRole('link', { name: 'Order' }).click();
-    await expect( page.getByRole('heading', { name: "Awesome is a click away" } ) ).toBeVisible();
-    await page.getByRole('link', { name: "Pepperoni"}).click();
-    await page.getByRole('link', { name: "Margarita"}).click();
-    await page.getByRole('link', { name: "Logout" } ).click();
+    await new Promise(resolve => setTimeout(resolve, 500));
 
-    // TODO: below was originally a second test.
-    await page.getByRole('link', { name: 'Login' }).click();
-    await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
-    await page.getByRole('textbox', { name: 'Email address' }).press('Tab');
-    await page.getByRole('textbox', { name: 'Password' }).fill('admin');
-    await page.getByRole('button', { name: 'Login' }).click();
+    // Order pizzas
+    await expect(page.getByRole('link', { name: 'Order' })).toBeVisible();
+    await page.getByRole('link', { name: 'Order' }).click();
+    await new Promise(resolve => setTimeout(resolve, 500));
+    await expect(page.getByText('Awesome is a click away')).toBeVisible();
+    await expect(page.getByText('What are you waiting for?')).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Image Description Pepperoni' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Image Description Margarita' })).toBeVisible();
+    await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();
+    await page.getByRole('link', { name: 'Image Description Margarita' }).click();
+    await page.getByRole('combobox').selectOption('1');
+    await page.getByRole('button', { name: 'Checkout' }).click();
+    await new Promise(resolve => setTimeout(resolve, 500));
+    await page.getByRole('button', { name: 'Pay now' }).click();
+    await page.getByRole('button', { name: 'Verify' }).click();
+    await expect(page.getByRole('button', { name: 'Close' })).toBeVisible();
+    await page.getByRole('button', { name: 'Close' }).click();
+    await page.getByRole('button', { name: 'Order more' }).click();
 
     // create a franchise
     await page.getByRole('link', { name: 'Admin' }).click();
@@ -50,46 +60,20 @@ test( "login and order pizza", async ( { page } ) => {
 
     // confirm store is made
     await expect( page.getByRole('cell', { name: randStore }) ).toBeVisible()
+    // close store
+    await page.getByRole('row', { name: `${randStore} 0 ₿ Close` }).getByRole('button').click();
+    await page.getByRole('button', { name: 'Close' }).click();
+    await new Promise(resolve => setTimeout(resolve, 500));
+    await page.getByRole('link', { name: 'Admin' }).click();
+    await page.getByRole('row', { name: `${randFranchise} 常用名字 Close` }).getByRole('button').click();
+    await page.getByRole('button', { name: 'Close' }).click();
+
+    await page.getByRole('link', { name: "Logout" } ).click();
 });
 
 function randomName() {
   return Math.random().toString(36).substring(2, 12);
 }
-
-// NOTE TO SELF: these tests all work individually, but the auth overlab is a problem. Dummy data will fix this.
-
-// test( "manage franchises and stores", async ( { page } ) => {
-//   // login
-//   await page.goto('http://localhost:5173/');
-//   await page.getByRole('link', { name: 'Login' }).click();
-//   await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
-//   await page.getByRole('textbox', { name: 'Email address' }).press('Tab');
-//   await page.getByRole('textbox', { name: 'Password' }).fill('admin');
-//   await page.getByRole('button', { name: 'Login' }).click();
-
-//   // create a franchise
-//   await page.getByRole('link', { name: 'Admin' }).click();
-//   await page.getByRole('button', { name: 'Add Franchise' }).click();
-//   await page.getByRole('textbox', { name: 'franchise name' }).click();
-//   const randFranchise = randomName()
-//   await page.getByRole('textbox', { name: 'franchise name' }).fill( randFranchise );
-//   await page.getByRole('textbox', { name: 'franchisee admin email' }).fill('a@jwt.com');
-//   await page.getByRole('button', { name: 'Create' }).click();
-
-//   // confirm franchise is made
-//   await expect( page.getByRole('cell', { name: randFranchise }) ).toBeVisible();
-
-//   // create a  store for the franchise
-//   await page.getByRole('link', { name: 'Franchise' }).click();
-//   await page.getByRole('button', { name: 'Create store' }).click();
-//   await page.getByRole('textbox', { name: 'store name' }).click();
-//   const randStore = randomName();
-//   await page.getByRole('textbox', { name: 'store name' }).fill( randStore );
-//   await page.getByRole('button', { name: 'Create' }).click();
-
-//   // confirm store is made
-//   await expect( page.getByRole('cell', { name: randStore }) ).toBeVisible()
-// } );
 
 test( "no auth pages", async ( { page } ) => {
   await page.goto('http://localhost:5173/');
@@ -123,4 +107,7 @@ test( "no auth pages", async ( { page } ) => {
   await expect(page.getByText('Welcome back')).toBeVisible();
   await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
   await expect(page.getByRole('main').getByText('Register')).toBeVisible();
+
+  // go to non existent page
+  // await page.goto( "http://locatlhost:5173/bad-page")
 } );
