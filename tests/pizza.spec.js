6f8593b639200ea1580fa75eb78c74909b57d5bb
ABCambridge <andrewbcambridge@gmail.com>
2025-02-12 09:47:47 -0700

    copied my old code and it fixes the percentage issue

--- tests/pizza.spec.js
+++ tests/pizza.spec.js
@@ -104,3 +104,66 @@ test( "create franchise and stores", async () => {
   await page.getByRole('link', { name: "Logout" } ).click();
   await expect( page.getByRole('link', { name: 'login' } ) ).toBeVisible()
 });
+
+test( "login and order pizza", async ( { page } ) => {
+  await page.goto('http://localhost:5173/');
+
+  // Login
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
+  await page.getByRole('textbox', { name: 'Email address' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin');
+  await page.getByRole('textbox', { name: 'Password' }).press('Enter');
+  await new Promise(resolve => setTimeout(resolve, 500));
+
+  // Order pizzas
+  await expect(page.getByRole('link', { name: 'Order' })).toBeVisible();
+  await page.getByRole('link', { name: 'Order' }).click();
+  await new Promise(resolve => setTimeout(resolve, 500));
+  await expect(page.getByText('Awesome is a click away')).toBeVisible();
+  await expect(page.getByText('What are you waiting for?')).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Image Description Pepperoni' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Image Description Margarita' })).toBeVisible();
+  await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();
+  await page.getByRole('link', { name: 'Image Description Margarita' }).click();
+  await page.getByRole('combobox').selectOption('1');
+  await page.getByRole('button', { name: 'Checkout' }).click();
+  await new Promise(resolve => setTimeout(resolve, 500));
+  await page.getByRole('button', { name: 'Pay now' }).click();
+  await page.getByRole('button', { name: 'Verify' }).click();
+  await expect(page.getByRole('button', { name: 'Close' })).toBeVisible();
+  await page.getByRole('button', { name: 'Close' }).click();
+  await page.getByRole('button', { name: 'Order more' }).click();
+
+  // create a franchise
+  await page.getByRole('link', { name: 'Admin' }).click();
+  await page.getByRole('button', { name: 'Add Franchise' }).click();
+  await page.getByRole('textbox', { name: 'franchise name' }).click();
+  const randFranchise = randomName()
+  await page.getByRole('textbox', { name: 'franchise name' }).fill( randFranchise );
+  await page.getByRole('textbox', { name: 'franchisee admin email' }).fill('a@jwt.com');
+  await page.getByRole('button', { name: 'Create' }).click();
+
+  // confirm franchise is made
+  await expect( page.getByRole('cell', { name: randFranchise }) ).toBeVisible();
+
+  // create a  store for the franchise
+  await page.getByRole('link', { name: 'Franchise' }).click();
+  await page.getByRole('button', { name: 'Create store' }).click();
+  await page.getByRole('textbox', { name: 'store name' }).click();
+  const randStore = randomName();
+  await page.getByRole('textbox', { name: 'store name' }).fill( randStore );
+  await page.getByRole('button', { name: 'Create' }).click();
+
+  // confirm store is made
+  await expect( page.getByRole('cell', { name: randStore }) ).toBeVisible()
+  // close store
+  await page.getByRole('row', { name: `${randStore} 0 ₿ Close` }).getByRole('button').click();
+  await page.getByRole('button', { name: 'Close' }).click();
+  await new Promise(resolve => setTimeout(resolve, 500));
+  await page.getByRole('link', { name: 'Admin' }).click();
+  await page.getByRole('row', { name: `${randFranchise} 常用名字 Close` }).getByRole('button').click();
+  await page.getByRole('button', { name: 'Close' }).click();
+
+  await page.getByRole('link', { name: "Logout" } ).click();
+});
